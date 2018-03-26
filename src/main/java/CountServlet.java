2f196fc98940dbfe1bb0f082f8e08066596f1d1c
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-26 16:24:39 -0500

    hello world and counter servlet initial commit

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+       private int count = 0;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        count++;
+        response.getWriter().println("The count is:" + " " + count);
+
+    }
+}
+
+
+
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) {
+            name = "World";
+        }
+
+        response.getWriter().println("Hello" + " " + name);
+//        response.getWriter().println("<p>Enter name:</p>");
+//        response.getWriter().println("<form>");
+//        response.getWriter().println("< input name=\"name\" />");
+//        response.getWriter().println("</form>");
+
+//        response.getWriter().println("Hello World!");
 
     }
 }
+
+
