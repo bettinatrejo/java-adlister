82bda70908b2680d7b7aa1cc651d15d4d3754fc4
Bettina Trejo <bettina.trejo12@gmail.com>
2018-04-06 14:39:31 -0500

    added Config

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package com.codeup.adlister.dao;
 
+
 public class DaoFactory {
     private static Ads adsDao;
     private static Users usersDao;
@@ -11,6 +12,7 @@ public class DaoFactory {
         }
         return adsDao;
     }
+
 
     public static Users getUsersDao() {
         if (usersDao == null) {
